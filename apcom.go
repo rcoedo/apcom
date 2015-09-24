5b038b6875ca5a1f38a1336856ead50bd17b3ad5
Ivan Lomba <ivanlomba@gmail.com>
2015-09-24 12:01:06 +0200

    All these are the beginning of sorrows

--- apcom.go
+++ apcom.go
@@ -8,10 +8,10 @@ var quotes = []string{
 	"Chaos will reign",
 	"One step closer to the end of the world",
 	"The Sun shall be turned into darkness",
-    "The whole land will be a burning waste of salt and sulfur; nothing planted, nothing sprouting, no vegetation growing on it.",
-    "There the fire will consume you, the sword will cut you down; they will devour you like a swarm of locusts.",
-    "All these are the beginning of sorrows",
-    "And many false prophets will appear and will deceive many people.",
+	"The whole land will be a burning waste of salt and sulfur; nothing planted, nothing sprouting, no vegetation growing on it.",
+	"There the fire will consume you, the sword will cut you down; they will devour you like a swarm of locusts.",
+	"All these are the beginning of sorrows",
+	"And many false prophets will appear and will deceive many people.",
 }
 
 func main() {
