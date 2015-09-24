78ba11f4c445d5e1377940a60ab541da185b7a9a
Roman Coedo <romancoedo@gmail.com>
2015-09-24 23:39:33 +0200

    As they dwell in darkness so shall I

--- apcom.go
+++ apcom.go
@@ -8,15 +8,17 @@ var quotes = []string{
 	"Chaos will reign",
 	"One step closer to the end of the world",
 	"The Sun shall be turned into darkness",
-	"The whole land will be a burning waste of salt and sulfur; nothing planted, nothing sprouting, no vegetation growing on it.",
-	"There the fire will consume you, the sword will cut you down; they will devour you like a swarm of locusts.",
+	"The whole land will be a burning waste of salt and sulfur; nothing planted, nothing sprouting, no vegetation growing on it",
+	"There the fire will consume you, the sword will cut you down; they will devour you like a swarm of locusts",
 	"All these are the beginning of sorrows",
-	"And many false prophets will appear and will deceive many people.",
+	"And many false prophets will appear and will deceive many people",
 	"Not even death can save you",
 	"Beware! Beyond lies mortal danger for the likes of you!",
 	"This holy place has been desecrated",
-	"Turn back! I can tell that you need more experience to fight safely in the next wilderness.",
-
+	"Turn back! I can tell that you need more experience to fight safely in the next wilderness",
+	"We shall feast when darkness falls, sing until our jaws are broken",
+	"Everyday’s like a war between the will to go on and a wish that the world would spiral into the sun",
+	"As they dwell in darkness so shall I",
 }
 
 func main() {
